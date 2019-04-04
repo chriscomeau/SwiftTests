5ea95845ad0df62cdc3b01c05422afc8b4cb954d
Chris Comeau <chris.comeau@skyriser.com>
2019-04-04 13:57:26 -0400

    post test, 2nd pass

--- PostTest/PostTest/ViewController.swift
+++ PostTest/PostTest/ViewController.swift
@@ -10,77 +10,71 @@ import UIKit
 
 class ViewController: UIViewController, URLSessionDelegate, URLSessionTaskDelegate, UITableViewDataSource, UITableViewDelegate {
   
-  
-  
   var session:URLSession? = nil
   
   @IBOutlet weak var tableView:UITableView?
   
-  //var tableArray = [String]()
   var tableArray = [Dictionary<String, Any>]()
-  /*    var test: Int
-   
-   
-   required init?(coder aDecoder: NSCoder) {
-   test = 0
-   super.init(coder: aDecoder)
-   }
-   */
   
-  
-  override func viewDidLoad() {
+    override func viewDidLoad() {
     super.viewDidLoad()
-    
-    getData()
+
     
     postData()
   }
   
-  
-  
-  //MARK: - URLSession -
-  
-  func getData() {
-    //get
-    session = URLSession.shared
-    let url:URL? = URL(string:"https://jsonplaceholder.typicode.com/posts")
-    
-    let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
-      guard error == nil else {
-        print("returned error")
-        return
-      }
-      
-      guard let content = data else {
-        print("No data")
-        return
-      }
-      
-      guard let json = (try? JSONSerialization.jsonObject(with: content, options:
-        JSONSerialization.ReadingOptions.mutableContainers)) as? [Dictionary<String, Any>] else {
-        print("Not containing JSON")
-        return
-      }
-      
-      self.tableArray = json
-      
-      print("tableArray: \(self.tableArray)")
-      
-      //table
-      
-      DispatchQueue.main.async {
-        //main thread
-        self.tableView!.reloadData()
-        
-      }
+  func postData() {
+    let todosEndpoint: String = "https://jsonplaceholder.typicode.com/todos"
+    guard let todosURL = URL(string: todosEndpoint) else {
+      print("Error: cannot create URL")
+      return
+    }
+    var todosUrlRequest = URLRequest(url: todosURL)
+    todosUrlRequest.httpMethod = "POST"
+    let newTodo: [String: Any] = ["title": "My First todo", "completed": false, "userId": 1]
+    let jsonTodo: Data
+    do {
+      jsonTodo = try JSONSerialization.data(withJSONObject: newTodo, options: [])
+      todosUrlRequest.httpBody = jsonTodo
+    } catch {
+      print("Error: cannot create JSON from todo")
+      return
     }
     
-    task.resume()
-  }
-  
-  func postData() {
+    let session = URLSession.shared
     
-    //https://jsonplaceholder.typicode.com
+    let task = session.dataTask(with: todosUrlRequest) {
+      (data, response, error) in
+      guard error == nil else {
+        print("error calling POST on /todos/1")
+        print(error!)
+        return
+      }
+      guard let responseData = data else {
+        print("Error: did not receive data")
+        return
+      }
+      
+      // parse the result as JSON, since that's what the API provides
+      do {
+        guard let receivedTodo = try JSONSerialization.jsonObject(with: responseData,
+                                                                  options: []) as? [String: Any] else {
+                                                                    print("Could not get JSON from responseData as dictionary")
+                                                                    return
+        }
+        print("The todo is: " + receivedTodo.description)
+        
+        guard let todoID = receivedTodo["id"] as? Int else {
+          print("Could not get todoID as int from JSON")
+          return
+        }
+        print("The ID is: \(todoID)")
+      } catch  {
+        print("error parsing response from POST on /todos")
+        return
+      }
+    }
+    task.resume()
   }
   
   //MARK: - table view -
@@ -100,13 +94,8 @@ class ViewController: UIViewController, URLSessionDelegate, URLSessionTaskDelega
       }
     }
     
-    //or
-    //cell.textLabel!.text = self.tableArray[indexPath.row]
-    
     return cell
     
   }
-  
-  
 }
 
